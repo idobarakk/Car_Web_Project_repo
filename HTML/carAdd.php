9c15c9208474a02220319e066788b00f8db37ca1
idobarakk <122865696+idobarakk@users.noreply.github.com>
2023-07-02 10:40:08 +0300

    Ziv Changes

---
+++ HTML/carAdd.php
@@ -0,0 +1,33 @@
+<?php
+$host = "localhost";
+$user = "zivma_zivmassad";
+$pass = "123456";
+$db = "zivma_carApp";
+$conn = new mysqli($host,$user,$pass,$db);
+
+if ($conn->connect_error){
+    die("connection failed:".$conn->connect_error);
+}
+$worker_num=$_POST['worker_num'];
+$car_company=$_POST['car_Company'];
+$car_model=$_POST['car_model'];
+$car_color=$_POST['car_color'];
+$license_num=$_POST['license_num'];
+$worker_first_name=$_POST['first_name'];
+$worker_last_name=$_POST['last_name'];
+$worker_Phone=$_POST['phone'];
+
+
+$sql="INSERT INTO `workers` (`worker_num`, `car_company`, `car_model`, `car_color`, `first_name`, `last_name`, `Phone_num`,`license_num`) VALUES ('".$worker_num."', '".$car_company."', '".$car_model."', '".$car_color."','".$worker_first_name."','".$worker_last_name."','".$worker_Phone."','".$license_num."' )";
+
+if ($conn->query($sql)==false){
+    echo "can not submit the data. Error is: ".$conn->error;
+}
+else
+echo "the data was submitted"
+?>
+
+
+
+
+?>
