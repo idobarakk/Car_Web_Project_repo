aa6f889f41b382919e5c08ca2ffe5a7d995380f3
idobarakk <122865696+idobarakk@users.noreply.github.com>
2023-07-04 17:35:56 +0300

    CSS Updat+ PHP remove

---
+++ removeCar.php
@@ -0,0 +1,27 @@
+<?php
+$host = "localhost";
+$user = "idobk_CarDB";
+$pass = "123456";
+$db = "idobk_CarDB";
+$conn = new mysqli($host,$user,$pass,$db);
+
+if ($conn->connect_error){
+    die("connection failed:".$conn->connect_error);
+}
+$carNumToRemove=$_POST['carNumToRemove'];
+
+
+
+$sql="DELETE FROM Cars WHERE license_num=:carNumToRemove;";
+
+if ($conn->query($sql)==false){
+    echo "can not submit the data. Error is: ".$conn->error;
+}
+else
+echo "the data was submitted"
+?>
+
+
+
+
+?>
